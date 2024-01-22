0412bd04f04e28b224dd90d19617619c041711ab
ramyus889 <ramyus889.com>
2024-01-22 11:13:58 +0300

    добавлен gh-pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/vue.js-pvtw - сайт/",
   plugins: [vue()],
   resolve: {
     alias: {
