5e47e95547d3c5329137ff51f88fb38aa4133609
ramyus889 <ramyus889.com>
2024-01-22 13:35:36 +0300

    добавлен gh-pages

--- src/main.js
+++ src/main.js
@@ -11,12 +11,9 @@ import "/css/fontStyle.css";
 import "/css/hover.css";
 import "/css/transform.css";
 import "./assets/main.css";
-import { autoAnimatePlugin } from "@formkit/auto-animate/vue";
 import { createApp } from "vue";
 import App from "./App.vue";
 
 const app = createApp(App);
-
-app.use(autoAnimatePlugin);
 
 app.mount("#app");
